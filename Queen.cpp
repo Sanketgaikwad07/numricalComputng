c36a4351a186932c4e55b08ffb5d411d64dd9473
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-12-23 20:29:56 +0530

    Update Queen.cpp

--- Queen.cpp
+++ Queen.cpp
@@ -8,6 +8,7 @@ bool isSafe(int row, int col, vector<string>& board, int n) {
         if (board[i][col] == 'Q') return false;
 
     // Check upper-left diagonal
+    
     for (int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--)
         if (board[i][j] == 'Q') return false;
 
